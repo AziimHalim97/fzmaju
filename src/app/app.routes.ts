92e25a2232358198efc5fdcd7a28fb2a525f8f7a
terbaik97 <aziimmashoor1997@gmail.com>
2025-12-10 22:44:26 +0800

    Add navbar and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from '../app/pages/home/home';
+import { AboutUs } from '../app/pages/about-us/about-us';
+import { Career } from '../app/pages/career/career';
+import { ContactUs } from '../app/pages/contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about-us', component: AboutUs },
+  { path: 'career', component: Career },
+  { path: 'contact-us', component: ContactUs },
+  { path: '**', redirectTo: '' } // fallback
+];
